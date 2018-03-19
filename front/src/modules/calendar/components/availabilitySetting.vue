d12a55b8442c1d06c4965604b460bac35a05af60
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-03-19 14:09:28 +0100

    add calendar component

---
+++ front/src/modules/calendar/components/availabilitySetting.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+ <h1>{{ msg }}</h1>
+ </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+  name: "availabilitySetting",
+  data() {
+    return {
+      msg: "availabilitySetting Vue"
+    };
+  },
+  components: {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
