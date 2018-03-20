943d2ec3bbe56ccf3990cc03f48f2f4963673db6
HanP77 <loic.aublet@gmail.com>
2018-03-20 10:09:36 +0100

    container in app deleted

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div class="container">
       <router-view></router-view>
-    </div>
   </div>
 </template>
 
@@ -33,6 +31,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
