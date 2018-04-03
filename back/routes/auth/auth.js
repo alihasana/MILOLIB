7594c02c619fef51da7c9ccee3fe247dba43219a
LuckyLaszlo <luke.perrey.dev@gmail.com>
2018-04-03 14:44:53 +0200

    On est sauvé

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,21 +1,18 @@
-import express from "express";
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import helper from "./../../helpers/helper";
-import User from "./../users/model";
+import express from 'express'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import helper from './../../helpers/helper';
+import User from './../users/model'
 
-let router = express.Router();
-loltest
-router.post("/login", (req, res) => {
+let router = express.Router()
+
+
+router.post('/login', (req, res) => {
   if (req.body.email && req.body.password) {
-    User.findOne({ email: helper.caseInsensitive(req.body.email) }, function(
-      err,
-      user
-    ) {
-      if (err) res.status(500).json({ success: false, message: err.message });
+    User.findOne({ email: helper.caseInsensitive(req.body.email) }, function (err, user) {
+      if (err) res.status(500).json({ success: false, message: err.message })
       if (!user) {
-
         res.status(401).json({ success: false, message: 'User not found.' })
       } else if (user) {
         if (!user.comparePasswords(req.body.password)) {
@@ -24,41 +21,37 @@ router.post("/login", (req, res) => {
           // JWT.SIGN(PAYLOAD, SECRETKEY, CALLBACK(err, result){...})
           jwt.sign({ email: user.email, _id: user._id }, process.env.SECRETKEY, function (err, result) {
             if (err) {
-              res.status(500).json({success: false, message: err.message})
+              res.status(500).json({ success: false, message: err.message })
             } else {
-              res.status(200).json({ success: true, message: 'Welcome !', content: {token: process.env.AUTHBEARER + ' ' + result}})
-            }
-          );
-        }
-      }
-    });
-  } else {
-    res.status(412).json({ success: false, message: 'email and/or password are missing.'})
-  }
-});
-
-router.post("/signup", (req, res) => {
-  if (req.body.email && req.body.password) {
-    if (helper.regexEmail.test(req.body.email)) {
-      User.findOne({ email: helper.caseInsensitive(req.body.email) }, function(
-        err,
-        result
-      ) {
-        if (result === null) {
-          let newUser = new User(req.body);
-          newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
-          newUser.save(function(err, user) {
-            if (err)
-              res.status(500).json({ success: false, message: err.message });
-            else {
-              helper.beforeSendUser(user)
-              res.status(200).json({ success: true, message: 'New user registered successfully!', content: user})
+              res.status(200).json({ success: true, message: 'Welcome !', content: { token: process.env.AUTHBEARER + ' ' + result } })
             }
           })
-        } else res.status(412).json({ success: false, message: 'email already used.'})
-      })
-    } else res.status(412).json({ success: false, message: 'Email required.' })
-  } else res.status(412).json({ success: false, message: 'Email and/or password are missing.'})
+        }
+      }
+    })
+  } else {
+    res.status(412).json({ success: false, message: 'email and/or password are missing.' })
+  }
 })
 
-export default router;
+router.post('/signup', (req, res) => {
+  if (req.body.email && req.body.password) {
+    if (helper.regexEmail.test(req.body.email)) {
+      User.findOne({ email: helper.caseInsensitive(req.body.email) }, function (err, result) {
+        if (result === null) {
+          let newUser = new User(req.body)
+          newUser.hashPassword = bcrypt.hashSync(req.body.password, 10)
+          newUser.save(function (err, user) {
+            if (err) res.status(500).json({ success: false, message: err.message })
+            else {
+              helper.beforeSendUser(user)
+              res.status(200).json({ success: true, message: 'New user registered successfully!', content: user })
+            }
+          })
+        } else res.status(412).json({ success: false, message: 'email already used.' })
+      })
+    } else res.status(412).json({ success: false, message: 'Email required.' })
+  } else res.status(412).json({ success: false, message: 'Email and/or password are missing.' })
+})
+
+export default router
