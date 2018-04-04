ac53190142af0628092e56d238fe9216ba8dbeb4
LuckyLaszlo <luke.perrey.dev@gmail.com>
2018-04-04 17:27:24 +0200

    WIP profile.put

--- back/routes/profile/controller.js
+++ back/routes/profile/controller.js
@@ -4,14 +4,28 @@ import User from './../users/model'
 import helper from './../../helpers/helper';
 
 export default {
-  updateProtected: () => {
+  protectedUpdate: () => {
+    // les 'res.status' ne sont peut-être pas correct (vue que l'on referra un plus tard dans la route), a voir.
     if (req.body.password || req.body.hashPassword) {
-      if (lol) {
-        
+      if ( !(req.body.oldPassword && req.body.oldPassword == res.locals.user.comparePasswords(req.body.oldPassword)) ) {
+        res.locals.user.unmarkModified('hashPassword')
+        res.status(401).json({ success: false, message: 'Actual password incorrect' })
+        resMessage1 = 'lol'
+      }
+    }
+    if (req.body.email) {
+      if ( !(req.body.oldEmail == res.locals.user.email) ) {
+        res.locals.user.unmarkModified('email')
+        res.status(401).json({ success: false, message: 'Actual email incorrect' }) 
       }
     }
     if (req.body.role) {
-
+      // Pas de condition pour le moment, on ne peux simplement pas modifier le data.role
+      res.locals.user.unmarkModified('role')
     }
+  },
+
+  test: () => {
+    return res.locals.message.resMessage1 = 'Actual password incorrect.'
   },
 }
--- back/routes/profile/profile.js
+++ back/routes/profile/profile.js
@@ -9,17 +9,60 @@ const ObjectId = mongoose.Types.ObjectId
 let router = express.Router()
 
 router.get('/', (req, res) => {
-  helper.beforeSendUser(res.locals.user)
+  // helper.beforeSendUser(res.locals.user)
   res.status(200).json({ success: true, message: 'Your profile.', content: res.locals.user })
 })
 
 // {runValidators : true}
 // exploiter le contenue de "result" pour faire des réponses differentes
+
+// router.put('/', (req, res) => {
+//   for (let key of Object.keys(req.body)) {
+//     res.locals.user[key] = req.body[key];
+//   }
+//   //
+//   var message = 'Profile updated.'
+
+//   // PROBLEMES DE SCOPE DES VARIABLES, A REVOIR
+
+//   // var resMessage1 = resMessage2 = resMessage3 = ''
+//   var resMessage1 = ''
+//   var resMessage2 = ''
+//   var resMessage3 = ''
+//   //
+//   if (req.body.password || req.body.hashPassword || req.body.role) {
+//     // controller.protectedUpdate()
+//     controller.test(resMessage1)
+//   }
+//   //
+//   //
+//   res.locals.user.update(req.body, (err, result) => {
+//     if (err) res.status(500).json({ success: false, message: err.message })
+//     else res.status(200).json({ success: true, message: message + resMessage1 + resMessage2 + resMessage3, content: result })
+//   })
+// })
+
 router.put('/', (req, res) => {
-  if (req.body.password || req.body.hashPassword || req.body.role) controller.updateProtected()
+  for (let key of Object.keys(req.body)) {
+    res.locals.user[key] = req.body[key];
+  }
+  //
+  var message = 'Profile updated.'
+  // res.locals.message = []
+  // res.locals.message.resMessage1 = ''
+  // res.locals.message.resMessage2 = ''
+  // res.locals.message.resMessage3 = ''
+  //
+  if (req.body.password || req.body.hashPassword || req.body.role) {
+    // controller.protectedUpdate()
+    controller.test()
+  }
+  //
+  //
   res.locals.user.update(req.body, (err, result) => {
     if (err) res.status(500).json({ success: false, message: err.message })
-    else res.status(200).json({ success: true, message: 'Profile updated!', content: result })
+    // else res.status(200).json({ success: true, message: message + res.locals.message.resMessage1 + res.locals.message.resMessage2 + res.locals.message.resMessage3, content: result })
+    else res.status(200).json({ success: true, response: res })
   })
 })
 
