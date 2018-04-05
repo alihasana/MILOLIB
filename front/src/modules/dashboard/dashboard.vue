c8f08c61248cece4a0b806d087adfd325c2d02d6
Kévin Beauguet <k.beauguet@gmail.com>
2018-04-05 14:42:07 +0200

    dashboard v2

--- front/src/modules/dashboard/dashboard.vue
+++ front/src/modules/dashboard/dashboard.vue
@@ -1,37 +1,58 @@
 <template>
   <div>
-    <b-navbar toggleable="sm" type="dark" variant="dark"> <!-- color: primary, success, info, warning, danger, dark, or light. -->
-      <b-navbar-brand href="#"><span><i class="material-icons">home</i></span>MILOLIB</b-navbar-brand>
+    
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-navbar-brand href="#"><span id="dashboard">DASHBOARD</span></b-navbar-brand>
       <b-navbar-toggle target="header_collapse"></b-navbar-toggle>
+    </b-navbar>
+        
+    <b-navbar type="dark" variant="dark" toggleable="sm" id="header_collapse">
       <b-collapse is-nav id="header_collapse">
-        <b-navbar-nav>
-          <b-nav-item href="#"><span><i class="material-icons">assignment_ind</i></span>User's list</b-nav-item>
-          <b-nav-item href="#"><span><i class="material-icons">date_range</i></span>Calendar</b-nav-item>
-        </b-navbar-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
+
+        <b-navbar-nav justified id="items"> <!-- fill or justified -->
+        
+          <!-- <b-nav-item href="#"><i class="material-icons">assignment_ind</i><span>User's list</span></b-nav-item> -->
+        
+          <!-- <b-nav-item href="#"><i class="material-icons">date_range</i><span>Calendar</span></b-nav-item> -->
+        
           <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
             <template slot="button-content">
-              <em><span><i class="material-icons">account_circle</i></span>Profile</em>
+              <em><i class="material-icons">assignment_ind</i><span>Users</span></em>
             </template>
-            <b-dropdown-item href="#"><span><i class="material-icons">settings</i></span>Settings</b-dropdown-item> <!-- <i class="material-icons">perm_data_setting</i> -->
-            <b-dropdown-item href="#"><span><i class="material-icons">power_settings_new</i></span>Logout</b-dropdown-item> <!-- <i class="material-icons">exit_to_app</i> -->
+            <b-dropdown-item href="#"><i class="material-icons">note_add</i><span>Create user</span></b-dropdown-item> 
+            <b-dropdown-item href="#"><i class="material-icons">folder_shared</i><span>User's list</span></b-dropdown-item>
+            <b-dropdown-item href="#"><i class="material-icons">perm_contact_calendar</i><span>User's schedule</span></b-dropdown-item>
           </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <template slot="button-content">
+              <em><i class="material-icons">date_range</i><span>Calendar</span></em>
+            </template>
+            <b-dropdown-item href="#"><i class="material-icons">perm_data_setting</i><span>Settings</span></b-dropdown-item> 
+            <b-dropdown-item href="#"><i class="material-icons">event</i><span>RDV</span></b-dropdown-item>
+            <b-dropdown-item href="#"><i class="material-icons">perm_contact_calendar</i><span>User's schedule</span></b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <template slot="button-content">
+              <em><i class="material-icons">account_circle</i><span>Profile</span></em>
+            </template>
+            <b-dropdown-item href="#"><i class="material-icons">question_answer</i><span>Messages</span></b-dropdown-item>
+            <b-dropdown-item href="#"><i class="material-icons">edit</i><span>Edit profile</span></b-dropdown-item>
+            <b-dropdown-item href="#"><i class="material-icons">settings</i><span>Settings</span></b-dropdown-item>
+            <b-dropdown-item href="#"><i class="material-icons">help</i><span>Help</span></b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item href="#"><i class="material-icons" id="logout">power_settings_new</i><span>Logout</span></b-dropdown-item> 
+          </b-nav-item-dropdown>
+
+
+          
+          
+          
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-    <b-navbar type="dark" variant="dark" id="aside_collapse">
-    <b-navbar-toggle target="aside_collapse"><span><i class="material-icons">dashboard</i></span>Dashboard</b-navbar-toggle>
-    <b-collapse is-nav id="aside_collapse">
-        <b-navbar-nav>
-          <b-nav-item href="#"><span><i class="material-icons">face</i></span>Create user</b-nav-item>
-          <b-nav-item href="#"><span><i class="material-icons">question_answer</i></span>Messages</b-nav-item>
-          <b-nav-item href="#"><span><i class="material-icons">folder</i></span>Module</b-nav-item>
-          <b-nav-item href="#" disabled><span><i class="material-icons">lock</i></span>Access denied</b-nav-item>
-        </b-navbar-nav>
-    </b-collapse>
-    </b-navbar>
+
   </div>
 </template>
 
@@ -55,15 +76,26 @@ export default {
 .material-icons.md-36 { font-size: 36px; }
 .material-icons.md-48 { font-size: 48px; }
 
-#aside_collapse {
-  width: 200px;
+#dashboard {
+  /* color: yellow; */
 }
 
-#header_collapse {
-  padding-left: 100px;
+#items {
+  /* background-color: red; */
+  width: 100%; /* comme les items sont en fill sur 100% de l'espace ils augmentent la taille de leur conteneur et décale ainsi le dropdown */
+
+}
+
+i {
+  /* color: purple; */
+  padding-right: 4px;
+}
+
+#logout {
+  color: red;
 }
 
 span {
-  padding-right: 4px;
+  /* color: purple; */
 }
 </style>
