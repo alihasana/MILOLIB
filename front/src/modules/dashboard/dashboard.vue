3761d98b35cb8747c2a13aebddc8b39d92ff0835
Kévin Beauguet <k.beauguet@gmail.com>
2018-04-05 15:21:46 +0200

    dashboard V2.1

--- front/src/modules/dashboard/dashboard.vue
+++ front/src/modules/dashboard/dashboard.vue
@@ -1,58 +1,47 @@
+<!-- Dashboard V2 : 
+- Problèmes :
+1) Les items sont en fill/justify [<b-navbar-nav justified id="items">] et occupe donc tous l'espace dispo mais le dropdown est décalé dû à l'agrandissement des divs.
+2) Le clic sur les dropdowns n'est pas parfait : marche si click sur l'icon ou le texte mais en dehors d'un certains périmètre le click fail et ne route pas.
+-->
 <template>
   <div>
-    
     <b-navbar toggleable="sm" type="dark" variant="dark">
       <b-navbar-brand href="#"><span id="dashboard">DASHBOARD</span></b-navbar-brand>
       <b-navbar-toggle target="header_collapse"></b-navbar-toggle>
     </b-navbar>
-        
     <b-navbar type="dark" variant="dark" toggleable="sm" id="header_collapse">
       <b-collapse is-nav id="header_collapse">
-
         <b-navbar-nav justified id="items"> <!-- fill or justified -->
-        
-          <!-- <b-nav-item href="#"><i class="material-icons">assignment_ind</i><span>User's list</span></b-nav-item> -->
-        
-          <!-- <b-nav-item href="#"><i class="material-icons">date_range</i><span>Calendar</span></b-nav-item> -->
-        
           <b-nav-item-dropdown right>
             <template slot="button-content">
               <em><i class="material-icons">assignment_ind</i><span>Users</span></em>
             </template>
-            <b-dropdown-item href="#"><i class="material-icons">note_add</i><span>Create user</span></b-dropdown-item> 
-            <b-dropdown-item href="#"><i class="material-icons">folder_shared</i><span>User's list</span></b-dropdown-item>
-            <b-dropdown-item href="#"><i class="material-icons">perm_contact_calendar</i><span>User's schedule</span></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'profileUser'}"><i class="material-icons">note_add</i><span>Create user</span></router-link></b-dropdown-item> 
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'userList'}"><i class="material-icons">folder_shared</i><span>User's list</span></router-link></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'example'}"><i class="material-icons">perm_contact_calendar</i><span>User's schedule</span></router-link></b-dropdown-item>
           </b-nav-item-dropdown>
-
           <b-nav-item-dropdown right>
             <template slot="button-content">
               <em><i class="material-icons">date_range</i><span>Calendar</span></em>
             </template>
-            <b-dropdown-item href="#"><i class="material-icons">perm_data_setting</i><span>Settings</span></b-dropdown-item> 
-            <b-dropdown-item href="#"><i class="material-icons">event</i><span>RDV</span></b-dropdown-item>
-            <b-dropdown-item href="#"><i class="material-icons">perm_contact_calendar</i><span>User's schedule</span></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'calendar'}"><i class="material-icons">perm_data_setting</i><span>Settings</span></router-link></b-dropdown-item> 
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'calendar'}"><i class="material-icons">event</i><span>RDV</span></router-link></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'example'}"><i class="material-icons">perm_contact_calendar</i><span>User's schedule</span></router-link></b-dropdown-item>
           </b-nav-item-dropdown>
-
           <b-nav-item-dropdown right>
             <template slot="button-content">
               <em><i class="material-icons">account_circle</i><span>Profile</span></em>
             </template>
-            <b-dropdown-item href="#"><i class="material-icons">question_answer</i><span>Messages</span></b-dropdown-item>
-            <b-dropdown-item href="#"><i class="material-icons">edit</i><span>Edit profile</span></b-dropdown-item>
-            <b-dropdown-item href="#"><i class="material-icons">settings</i><span>Settings</span></b-dropdown-item>
-            <b-dropdown-item href="#"><i class="material-icons">help</i><span>Help</span></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'example'}"><i class="material-icons">question_answer</i><span>Messages</span></router-link></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'example'}"><i class="material-icons">edit</i><span>Edit profile</span></router-link></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'example'}"><i class="material-icons">settings</i><span>Settings</span></router-link></b-dropdown-item>
+            <b-dropdown-item ><router-link tag="span" :to="{name: 'example'}"><i class="material-icons">help</i><span>Help</span></router-link></b-dropdown-item>
             <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item href="#"><i class="material-icons" id="logout">power_settings_new</i><span>Logout</span></b-dropdown-item> 
+            <b-dropdown-item><router-link tag="span" :to="{name: 'login'}"><i class="material-icons" id="logout">power_settings_new</i><span>Logout</span></router-link></b-dropdown-item> 
           </b-nav-item-dropdown>
-
-
-          
-          
-          
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-
   </div>
 </template>
 
@@ -95,7 +84,13 @@ i {
   color: red;
 }
 
-span {
-  /* color: purple; */
+/* span {
+  color: #16181B;
+  text-decoration: none;
 }
+
+span:hover {
+  color: #16181B;
+  text-decoration: none;
+} */
 </style>
