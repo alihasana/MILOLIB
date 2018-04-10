7bd5ce2e9c7ded03749f8cd6c84d579ba69fcb30
HanP77 <loic.aublet@gmail.com>
2018-04-10 17:08:45 +0200

    some minors changes

--- front/src/modules/auth/Login.vue
+++ front/src/modules/auth/Login.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div class="login">
    <form v-on:submit.prevent>
-    <div class="row">
+    <div class="row login__row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1 class="heading-primary">{{ title }}</h1>
         <hr>
