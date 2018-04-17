f8f86639f4bc5cf7abd15bc00bae9f4ccb9033c2
HanP77 <loic.aublet@gmail.com>
2018-04-17 15:44:21 +0200

    sendgrid/email added in the backend. But we need to create an account with the email created for milolib

---
+++ back/middlewares/mail.js
@@ -0,0 +1,9 @@
+import sgMail from '@sendgrid/mail'
+// creer un helper a la place ?
+let sendMail = (receiverID, content) => {
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  content.to = receiverID
+  sgMail.send(content)
+}
+
+export default sendMail;
