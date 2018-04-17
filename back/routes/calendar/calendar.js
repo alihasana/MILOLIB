8027f02a84574371ed02eafdc18a2e799589d908
LuckyLaszlo <luke.perrey.dev@gmail.com>
2018-04-17 10:33:56 +0200

    slots test

--- back/routes/calendar/calendar.js
+++ back/routes/calendar/calendar.js
@@ -6,14 +6,15 @@ import mongoose from 'mongoose'
 let router = express.Router()
 
 router.post('/', (req, res) => {
-
   for (let key of Object.keys(req.body)) {
     res.locals.user.calendar.slots.push(req.body[key])
   }
+  res.locals.user.calendar.slots.markModified('start')
+  res.locals.user.calendar.slots.markModified('end')
 
   res.locals.user.save((err) => {
     if (err) res.status(500).json({ success: false, message: err.message })
-    else res.status(200).json({ success: true, message: 'C\'est ok. Tout va bien' })
+    else res.status(200).json({ success: true, message: 'C\'est ok. Slots ajoutées' })
   })
 })
 
--- back/schema/slot.js
+++ back/schema/slot.js
@@ -2,8 +2,8 @@
 import mongoose from 'mongoose'
 
 let SlotSchema = new mongoose.Schema({
-  start: String ,
-  end: String ,
+  start: mongoose.Schema.Types.Mixed,
+  end: mongoose.Schema.Types.Mixed,
   available: { type: Boolean, required: true, default: true },
   // status: { type: String },
   // OR
