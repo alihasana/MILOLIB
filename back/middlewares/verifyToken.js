b513fab42771cb878a00c30ff66fa204d089d8a7
HanP77 <loic.aublet@gmail.com>
2018-04-23 16:10:36 +0200

    test token

--- back/middlewares/verifyToken.js
+++ back/middlewares/verifyToken.js
@@ -1,7 +1,8 @@
-import express from "express"
-import mongoose from "mongoose"
-import jwt from "jsonwebtoken"
+import express from 'express'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
 import User from './../routes/users/model'
+import Client from './../routes/clients/model'
 const ObjectId = mongoose.Types.ObjectId
 
 // la premiere partie est égale à un mot defini dans la config, et la seconde est égale au token.
@@ -9,28 +10,48 @@ const ObjectId = mongoose.Types.ObjectId
 // Ce dernier correspond au token décodé, on retrouve le payload (ex: email utilisateur, id etc..)
 // on appelle next() pour dire que tout s'est bien passé et qu'on peut passer à la suite (circulez svp!)
 let verifyToken = (req, res, next) => {
-  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === process.env.AUTHBEARER) {
-    jwt.verify(req.headers.authorization.split(' ')[1], process.env.SECRETKEY, (err, decode) => {
-      if (err) res.status(500).json({ success: false, message: err.message })
-      else {
-        // res.locals permet de stocker des datas utilisable dans la requête en cours
-        res.locals.decode = decode
-        if (ObjectId.isValid(decode._id)) {
-          if (decode.userCollection == 'User' || 'Client') {
-            eval(decode.userCollection).findById(decode._id, (err, user) => {
-              if (err) res.status(500).json({ success: false, message: err.message })
-              else if (!user) res.status(401).json({ success: false, message: 'Unauthozired' })
-              else if (user.active !== true) res.status(403).json({ success: false, message: 'Inactive account. Please contact an administrator.' })
-              else {
-                res.locals.user = user
-                // res.locals.userUnmodified = JSON.parse(JSON.stringify(user)) // Clone of user for verification purpose
-                next()
-              }
-            })
-          } else res.status(400).json({ success: false, message: 'Bad request' })
-        } else res.status(400).json({ success: false, message: 'Invalid ID' })
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === process.env.AUTHBEARER
+  ) {
+    jwt.verify(
+      req.headers.authorization.split(' ')[1],
+      process.env.SECRETKEY,
+      (err, decode) => {
+        if (err) res.status(500).json({ success: false, message: err.message })
+        else {
+          // res.locals permet de stocker des datas utilisable dans la requête en cours
+          res.locals.decode = decode
+          if (ObjectId.isValid(decode._id)) {
+            if (decode.userCollection == 'User' || 'Client') {
+              eval(decode.userCollection).findById(decode._id, (err, user) => {
+                if (err)
+                  res.status(500).json({ success: false, message: err.message })
+                else if (!user)
+                  res
+                    .status(401)
+                    .json({ success: false, message: 'Unauthozired' })
+                else if (user.active !== true)
+                  res
+                    .status(403)
+                    .json({
+                      success: false,
+                      message:
+                        'Inactive account. Please contact an administrator.'
+                    })
+                else {
+                  res.locals.user = user
+                  // res.locals.userUnmodified = JSON.parse(JSON.stringify(user)) // Clone of user for verification purpose
+                  next()
+                }
+              })
+            } else
+              res.status(400).json({ success: false, message: 'Bad request' })
+          } else res.status(400).json({ success: false, message: 'Invalid ID' })
+        }
       }
-    })
+    )
   } else res.status(401).json({ success: false, message: 'Unauthozired' })
 }
 
