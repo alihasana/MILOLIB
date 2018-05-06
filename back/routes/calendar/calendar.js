6f6130de4c382ae25fe466ee4c337492a172f562
LuckyLaszlo <luke.perrey.dev@gmail.com>
2018-05-06 21:19:26 +0200

    added /:userId calendar routes (for the 'Chargé d\'accueil' )

--- back/routes/calendar/calendar.js
+++ back/routes/calendar/calendar.js
@@ -6,6 +6,7 @@ import Calendar from './model'
 import Appointment from './../../models/appointment'
 import controller from './controller'
 import helper from '../../helpers/helper'
+const ObjectId = mongoose.Types.ObjectId
 // import util from 'util'
 
 let router = express.Router()
@@ -14,83 +15,61 @@ let router = express.Router()
 router.post('/', (req, res) => {
   // console.log('Le req.body ' + JSON.stringify(req.body, null, 4))
   Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
-    if (err) res.status(500).json({ success: false, message: err.message })
-    else {
-      console.log('Le calendar find one ' + JSON.stringify(calendar, null, 4))
+    if (err) return res.status(500).json({ success: false, message: err.message })
 
-      // //WIP, async problems
-      // if (!calendar) { // Create calendar if needed
-      //   calendar = controller.asyncCall(res.locals);
-      // }
-
-
-      // TODO : a retest, aprés création du calendar le 'controller.postCalendar(req, res, calendar)' ne marche pas.
-      if (!calendar) { // Create calendar if needed
-        controller.createCalendar(res.locals)
-          .then(function (calendar) {
-            controller.postCalendar(req, res, calendar)
-          })
-          .catch(err => {
-            return res.status(500).json({ success: false, message: err.message })
-          })
-      } else controller.postCalendar(req, res, calendar)
-
-    }
+    // TODO : a retest, aprés création du calendar le 'controller.postCalendar(req, res, calendar)' ne marche pas.
+    if (!calendar) { // Create calendar if needed
+      controller.createCalendar(res.locals)
+        .then(function (calendar) {
+          controller.postCalendar(req, res, calendar)
+        })
+        .catch(err => {
+          return res.status(500).json({ success: false, message: err.message })
+        })
+    } else controller.postCalendar(req, res, calendar)
   })
 })
 
-// router.post('/OLDSCHOOL', (req, res) => {
-//   // console.log('Le req.body ' + JSON.stringify(req.body, null, 4))
-//   Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
-//     if (err) res.status(500).json({ success: false, message: err.message })
-//     else {
-//       console.log('Le calendar find one ' + JSON.stringify(calendar, null, 4))
 
-//       // //WIP, async problems
-//       if (!calendar) { // Create calendar if needed
-//         calendar = controller.asyncCall(res.locals);
-//       }
+router.post('/:userId', (req, res) => {
+  if (res.locals.user.role != 'Chargé d\'accueil') return res.status(403).json({ succes: false, message: 'Forbidden.' })
+  if (!ObjectId.isValid(req.params.userId)) return res.status(400).json({ success: false, message: 'Invalid ID' })
 
-//       // Verify slots conflicts 
-//       for (let key of Object.keys(req.body)) {
-//         if (controller.checkSlotsConflict(calendar.slots, req.body[key].start)) {
-//           return res.status(400).json({ success: false, message: 'Your slots request conflict with slots already present in the calendar' })
-//         }
-//       }
+  Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
+    if (err) return res.status(500).json({ success: false, message: err.message })
+    else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
 
-//       // ADD Slots
-//       for (let key of Object.keys(req.body)) {
-//         calendar.slots.push(req.body[key])
-//       }
-
-//       // TODO: A voir si 'content: calendar' ou si pas besoin de content dans la réponse.
-//       calendar.save((err, calendar) => {
-//         if (err) res.status(500).json({ success: false, message: err.message })
-//         else res.status(200).json({ success: true, message: 'C\'est ok. Slots ajoutées', content: calendar })
-//       })
-//     }
-//   })
-// })
+    controller.postCalendar(req, res, calendar)
+  })
+})
 
 
 router.get('/', (req, res) => {
   Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
     if (err) return res.status(500).json({ success: false, message: err.message })
     else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
-    // TODO: A voir comment filter les slots lors du .find()
-    // Filtre provisoire
-    // PLUS DE FILTRE
-    // for (let key of Object.keys(calendar.slots)) {
-    //   if (calendar.slots[key].available !== true) {
-    //     delete calendar.slots[key]
-    //   }
-    // }
-    // TODO: add 'Appointments' list and merge it with calendar before send.
+
     res.status(200).json({ success: true, message: 'Your calendar.', content: calendar })
   })
 })
 
-// TOVIEW: ROUTE UTILE ?
+
+router.get('/:userId', (req, res) => {
+  if (res.locals.user.role != 'Chargé d\'accueil') return res.status(403).json({ succes: false, message: 'Forbidden.' })
+  if (!ObjectId.isValid(req.params.userId)) return res.status(400).json({ success: false, message: 'Invalid ID' })
+
+  Calendar.findOne({ userId: req.params.userId })
+    .populate('userId')
+    .exec((err, calendar) => {
+      if (err) return res.status(500).json({ success: false, message: err.message })
+      else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
+
+      helper.beforeSendUser(calendar.userId)
+      res.status(200).json({ success: true, message: 'Calendar of ' + calendar.userId.email, content: calendar })
+    })
+})
+
+
 router.get('/appointmentTypes', (req, res) => {
   Calendar.findOne({ userId: res.locals.user.id }, appointmentTypes, (err, calendar) => {
     if (err) return res.status(500).json({ success: false, message: err.message })
@@ -99,6 +78,18 @@ router.get('/appointmentTypes', (req, res) => {
     res.status(200).json({ success: true, message: 'Appointment types list', content: calendar.appointmentTypes })
   })
 })
+router.get('/:userId/appointmentTypes', (req, res) => {
+  if (res.locals.user.role != 'Chargé d\'accueil') return res.status(403).json({ succes: false, message: 'Forbidden.' })
+  if (!ObjectId.isValid(req.params.userId)) return res.status(400).json({ success: false, message: 'Invalid ID' })  
+
+  Calendar.findOne({ userId: req.params.userId }, appointmentTypes, (err, calendar) => {
+    if (err) return res.status(500).json({ success: false, message: err.message })
+    else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
+
+    res.status(200).json({ success: true, message: 'Appointment types list', content: calendar.appointmentTypes })
+  })
+})
+
 
 router.put('/appointmentTypes', (req, res) => {
   Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
@@ -118,34 +109,59 @@ router.put('/appointmentTypes', (req, res) => {
 
     calendar.appointmentTypes = req.body
     calendar.save((err) => {
-      if (err) res.status(500).json({ success: false, message: err.message })
-      // TOVIEW : content necessaire ? apriori non
-      else res.status(200).json({ success: true, message: 'Appointment Types modifiés', content: calendar })
+      if (err) return res.status(500).json({ success: false, message: err.message })
+      res.status(200).json({ success: true, message: 'Appointment Types modifiés'})
     })
   })
 })
-const ObjectId = mongoose.Types.ObjectId
 
-// TODO: test la route
-router.get('/appointment/:slotId', (req, res) => {
-  if (!ObjectId.isValid(req.params.slotId)) return res.status(400).json({ success: false, message: 'Invalid ID' })
-  Appointment.findOne({ "slots._id": req.params.slotId})
-  .populate('participants.clients') // TODO: VIRER LE PASSWORD (pre.find?), IMPORTANT !
-  .populate('participants.staff') // TODO: VIRER LE PASSWORD, (pre.find?)IMPORTANT !
-  .exec((err, appointment) => {
+router.put('/:userId/appointmentTypes', (req, res) => {
+  if (res.locals.user.role != 'Chargé d\'accueil') return res.status(403).json({ succes: false, message: 'Forbidden.' })
+  if (!ObjectId.isValid(req.params.userId)) return res.status(400).json({ success: false, message: 'Invalid ID' })  
+
+  Calendar.findOne({ userId: req.params.userId }, (err, calendar) => {
     if (err) return res.status(500).json({ success: false, message: err.message })
-    else if (!appointment) return res.status(404).json({ success: false, message: 'Appointment not found' })
+    else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
 
-    helper.beforeSendUser(appointment.participants.staff)
-    for (let key of Object.keys(appointment.participants.clients)) {
-      helper.beforeSendUser(key)
+    if (!req.body && !req.body[0]) {
+      return res.status(400).json({ success: false, message: 'Bad request' })
     }
 
-    res.status(200).json({ success: true, message: 'Your appointment.', content: appointment })
+    for (let key of Object.keys(req.body)) {
+      if (!req.body[key].name && !req.body[key].duration) {
+        return res.status(400).json({ success: false, message: 'Bad request' })
+        // TODO: duration en nombres de slots plutot qu'en minutes ?
+      }
+    }
+
+    calendar.appointmentTypes = req.body
+    calendar.save((err) => {
+      if (err) return res.status(500).json({ success: false, message: err.message })
+      res.status(200).json({ success: true, message: 'Appointment Types modifiés'})
+    })
   })
 })
 
-// ERROR TODO : "Calendar validation failed: slots.14.appointment.appointmentType: Cast to String failed for value "{ name: 'Emploi', duration: '30' }" at path "appointment.appointmentType", slots.15.appointment.appointmentType: Cast to String failed for value "{ name: 'Emploi', duration: '30' }" at path "appointment.appointmentType""
+
+router.get('/appointment/:slotId', (req, res) => {
+  if (!ObjectId.isValid(req.params.slotId)) return res.status(400).json({ success: false, message: 'Invalid ID' })
+
+  Appointment.findOne({ "slots._id": req.params.slotId })
+    .populate('participants.clients') // TODO: VIRER LE PASSWORD de façon global (schema)
+    .populate('participants.staff') // TODO: VIRER LE PASSWORDde façon global (schema)
+    .exec((err, appointment) => {
+      if (err) return res.status(500).json({ success: false, message: err.message })
+      else if (!appointment) return res.status(404).json({ success: false, message: 'Appointment not found' })
+
+      helper.beforeSendUser(appointment.participants.staff)
+      for (let key of Object.keys(appointment.participants.clients)) {
+        helper.beforeSendUser(key)
+      }
+
+      res.status(200).json({ success: true, message: 'Your appointment.', content: appointment })
+    })
+})
+
 
 router.post('/appointment', (req, res) => {
   // req.body est un array d'id de slots OU array de dates ? + id du calendar.
@@ -179,7 +195,7 @@ router.post('/appointment', (req, res) => {
           // Make slot unavailable in calendar
           calendar.slots.id(req.body.slotsId[key]).available = false
           calendar.slots.id(req.body.slotsId[key]).appointment = {
-            fullName: client.firstName + client.lastName,
+            fullName: client.firstName + ' ' + client.lastName,
             // fullName: client.firstName,
             appointmentType: req.body.appointmentType,
           }
@@ -209,29 +225,85 @@ router.post('/appointment', (req, res) => {
   })
 })
 
+router.post('/:userId/appointment', (req, res) => {
+  if (res.locals.user.role != 'Chargé d\'accueil') return res.status(403).json({ succes: false, message: 'Forbidden.' })
+  if (!ObjectId.isValid(req.params.userId)) return res.status(400).json({ success: false, message: 'Invalid ID' })  
+
+  Client.findOne({ email: req.body.mailClient }, (err, client) => {
+    if (err) return res.status(500).json({ success: false, message: err.message })
+    else if (!client) return res.status(404).json({ success: false, message: 'Bad email, client not found' })
+
+    Calendar.findOne({ userId: req.params.userId }, (err, calendar) => {
+      if (err) return res.status(500).json({ success: false, message: err.message })
+      else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
+
+      if (!req.body && !req.body.slotsId && !req.body.slotsId[0] && req.body.appointmentType) {
+        return res.status(400).json({ success: false, message: 'Bad request' })
+      }
+
+      var appointmentSlots = []
+      for (let key of Object.keys(req.body.slotsId)) {
+        if (calendar.slots.id(req.body.slotsId[key]) != null) {
+          // Check conflict
+          if (calendar.slots.id(req.body.slotsId[key]).available == false) {
+            return res.status(400).json({ success: false, message: 'Cannot create appointement, slots unavailable' })
+          }
+          // Make slot unavailable in calendar
+          calendar.slots.id(req.body.slotsId[key]).available = false
+          calendar.slots.id(req.body.slotsId[key]).appointment = {
+            fullName: client.firstName + ' ' + client.lastName,
+            // fullName: client.firstName,
+            appointmentType: req.body.appointmentType,
+          }
+          // Add slot to 'appointmentSlots' array
+          appointmentSlots.push(calendar.slots.id(req.body.slotsId[key]))
+        }
+      }
+
+      calendar.save((err, calendar) => {
+        if (err) return res.status(500).json({ success: false, message: err.message })
+        let newAppointment = new Appointment({
+          appointmentType: req.body.appointmentType,
+          participants: {
+            clients: [client.id], // TODO: verif si array nécessaires
+            staff: req.params.userId,
+          },
+          slots: appointmentSlots,
+          description: req.body.description,
+        })
+
+        newAppointment.save((err, appointment) => {
+          if (err) return res.status(500).json({ success: false, message: err.message })
+          res.status(200).json({ success: true, message: 'New Appointment successfully created!', content: appointment })
+        })
+      })
+    })
+  })
+})
+
 // -------------------------------------------------------------------
 //                        NON FONCTIONNELS 
 // -------------------------------------------------------------------
 
-router.get('/testFind', (req, res) => {
-  Calendar.find({}, (err, calendars) => {
-    if (err) return res.status(500).json({ success: false, message: err.message })
-    else if (!calendars) return res.status(404).json({ success: false, message: 'Calendars not found' })
-    else {
-      // TODO : filtrer les slots not available
-      res.status(200).json({ success: true, message: 'Calendars with available appointments.', content: calendars })
-    }
-  })
-})
+// router.get('/testFind', (req, res) => {
+//   Calendar.find({}, (err, calendars) => {
+//     if (err) return res.status(500).json({ success: false, message: err.message })
+//     else if (!calendars) return res.status(404).json({ success: false, message: 'Calendars not found' })
+//     else {
+//       // TODO : filtrer les slots not available
+//       res.status(200).json({ success: true, message: 'Calendars with available appointments.', content: calendars })
+//     }
+//   })
+// })
 
-//Populate test
-router.get('/testPopulate', (req, res) => {
-  Calendar.findOne({ userId: res.locals.user.id }).populate('userId').exec((err, calendar) => {
-    if (err) return res.status(500).json({ success: false, message: err.message })
-    else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
-    res.status(200).json({ success: true, message: 'Your calendar.', content: calendar })
-  })
-})
+// //Populate test
+// router.get('/testPopulate', (req, res) => {
+//   Calendar.findOne({ userId: res.locals.user.id }).populate('userId').exec((err, calendar) => {
+//     if (err) return res.status(500).json({ success: false, message: err.message })
+//     else if (!calendar) return res.status(404).json({ success: false, message: 'Calendar not found' })
+//     res.status(200).json({ success: true, message: 'Your calendar.', content: calendar })
+//   })
+// })
 
 // router.get('/test', (req, res) => {
 //   Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
@@ -297,6 +369,39 @@ router.get('/testPopulate', (req, res) => {
 //   res.locals.user.save((err) => {
 //     if (err) res.status(500).json({ success: false, message: err.message })
 //     else res.status(200).json({ success: true, message: 'C\'est ok. Slots ajoutées' })
+//   })
+// })
+
+// router.post('/OLDSCHOOL', (req, res) => {
+//   // console.log('Le req.body ' + JSON.stringify(req.body, null, 4))
+//   Calendar.findOne({ userId: res.locals.user.id }, (err, calendar) => {
+//     if (err) res.status(500).json({ success: false, message: err.message })
+//     else {
+//       console.log('Le calendar find one ' + JSON.stringify(calendar, null, 4))
+
+//       // //WIP, async problems
+//       if (!calendar) { // Create calendar if needed
+//         calendar = controller.asyncCall(res.locals);
+//       }
+
+//       // Verify slots conflicts 
+//       for (let key of Object.keys(req.body)) {
+//         if (controller.checkSlotsConflict(calendar.slots, req.body[key].start)) {
+//           return res.status(400).json({ success: false, message: 'Your slots request conflict with slots already present in the calendar' })
+//         }
+//       }
+
+//       // ADD Slots
+//       for (let key of Object.keys(req.body)) {
+//         calendar.slots.push(req.body[key])
+//       }
+
+//       // TODO: A voir si 'content: calendar' ou si pas besoin de content dans la réponse.
+//       calendar.save((err, calendar) => {
+//         if (err) res.status(500).json({ success: false, message: err.message })
+//         else res.status(200).json({ success: true, message: 'C\'est ok. Slots ajoutées', content: calendar })
+//       })
+//     }
 //   })
 // })
 
