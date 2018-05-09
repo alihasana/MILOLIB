32e65b68f9e0adb00e045d8d93278fa174d62b8e
Asma <elkabir.asma@gmail.com>
2018-05-09 11:56:44 +0200

    create-user:ok for the umpteenth time

--- front/src/modules/users/components/CreateUser.vue
+++ front/src/modules/users/components/CreateUser.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="createUser">
-    <div class="createUser__row">
+  <div>
+    <div>
       <h1>{{ title }}</h1>
       <!-- Create user Form -->
-      <div class="createUser__form">
-        <b-form>
-<!-- Name -->
+      <div>
+        <b-form @submit="createUser">
+        <!-- Name -->
           <b-form-group id="">
             <b-form-input id="" v-model="form.lName" required placeholder="Entrer le nom du nouvel utilisateur">
             </b-form-input>
@@ -25,17 +25,17 @@
             <b-form-select id="" :options="userRole" required v-model="form.userRole">
             </b-form-select>
           </b-form-group>
-    <b-form-group label="Indiquer le lieu d'exercice du nouvel utilisateur">
-      <b-form-checkbox-group id="workPlace" name="workPlace" v-model="form.place" :options="options">
-      </b-form-checkbox-group>
-    </b-form-group>
+          <b-form-group label="Indiquer le lieu d'exercice du nouvel utilisateur">
+            <b-form-checkbox-group id="workPlace" name="workPlace" v-model="form.place" :options="workPlace">
+            </b-form-checkbox-group>
+          </b-form-group>
           <!-- Password -->
           <b-form-group>
             <b-form-input v-model="form.userPassword" placeholder="Entrer un mot de passe"></b-form-input>
           </b-form-group>
           <!-- Admin's actions buttons -->
           <b-button-group>
-            <b-button v-on:click="createUser" type="submit" variant="success">Enregistrer</b-button>
+            <b-button type="submit" variant="success">Enregistrer</b-button>
           </b-button-group>
         </b-form>
       </div>
@@ -48,8 +48,8 @@
 /* eslint-disable */
 import swal from "sweetalert2";
 import http from "../../../helpers/http";
-export default {
 
+export default {
   name: "createUser",
   data() {
     return {
@@ -62,23 +62,24 @@ export default {
         userRole: null,
         place: null,
       },
-      // User:{}, 
       userRole: [{
         text: 'Assigner un rôle au nouvel utilisateur',
         value: null
       },
-      'Administrateur', 'Administrateur-Conseiller', 'Conseiller', 'Chargé d\'accueil', 'Invité'
+      'Administrateur', 'Administrateur/Conseiller', 'Conseiller', 'Chargé d\'accueil', 'Invité'
       ],
-      options: [
-        {text: 'Sénart', value: 'Sénart'},
+      workPlace: [
+        {text: 'MDEF', value: 'MDEF'},
         {text: 'Combs-La-Ville', value: 'Combs-La-Ville'},
-        {text: 'Moissy-Cramayel', value: 'Moissy-Cramayel'}
-
+        {text: 'Saint-Pierre-Du-Perray', value: 'Saint-Pierre-Du-Perray'},
+        {text: 'Tigéry', value: 'Tigéry'}
       ]
     }
   },
   methods: {
-     createUser() {
+     createUser(evt) {
+      evt.preventDefault();
+      console.log('welcome to createUser function')
       const User = {
         lastName: this.form.lName, 
         firstName: this.form.fName,
@@ -87,12 +88,10 @@ export default {
         password: this.form.userPassword, 
         workPlace: this.form.place
       }
-      console.log('Object from parent: ', User)
+      console.log('Object from CreateUser component: ', User)
       http.post('/users', User)
-      console.log('profil créé: ', User)
-
-
       .then(res => {
+      console.log('profil créé: ', User)
         swal({
               type: "success",
               title: "L'utilisateur a bien été créé !",
